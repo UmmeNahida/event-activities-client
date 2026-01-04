4ccfb402c89816ff901d658e613d6a1f8cc8a4bd
Nahida Akther <ummenahida20@gmail.com>
2026-01-04 22:08:58 +0600

    feat(event-details): add participant section

--- types/event-details.interface.ts
+++ types/event-details.interface.ts
@@ -1,5 +1,32 @@
-// Host Interface
-export interface IHost {
+export interface EventDetailsResponse {
+  success: boolean;
+  message: string;
+  data: EventDetails;
+}
+
+export interface EventDetails {
+  id: string;
+  name: string;
+  type: string;
+  description: string;
+  location: string;
+  image: string;
+  date: string;
+  time: string;
+  minParticipants: number;
+  maxParticipants: number;
+  participantCount: number;
+  fee: number;
+  status: "OPEN" | "CLOSED" | "COMPLETED";
+  createdAt: string;
+  hostId: string;
+  host: Host;
+  participants: Participant[];
+  reviews: Review[];
+  payments: Payment[];
+}
+
+export interface Host {
   id: string;
   name: string;
   image: string;
@@ -7,62 +34,31 @@ export interface IHost {
   bio: string;
 }
 
-// Participant (basic – প্রয়োজন অনুযায়ী extend করতে পারো)
-export interface IParticipant {
+export interface Participant {
   id: string;
   userId: string;
   eventId: string;
-  createdAt: string; // or Date
+  paid: boolean;
+  joinedAt: string;
+  user: User;
 }
 
-// Review (future-proof)
-export interface IReview {
-  id: string;
-  rating: number;
-  comment?: string;
-  userId: string;
-  eventId: string;
-  createdAt: string; // or Date
-}
-
-// Payment
-export interface IPayment {
-  id: string;
-  amount: number;
-  status: "PENDING" | "SUCCESS" | "FAILED";
-  userId: string;
-  eventId: string;
-  createdAt: string; // or Date
-}
-
-// Main Event Interface
-export interface IEvent {
+export interface User {
   id: string;
   name: string;
-  type: "Sports" | "Music" | "Tech" | "Art" | string;
-  description: string;
-  location: string;
   image: string;
-
-  date: string; // ISO Date string (2025-12-17T16:00:00.000Z)
-  time: string; // "22:00"
-
-  minParticipants: number;
-  maxParticipants: number;
-  participantCount: number;
-  isPaid?:boolean
-
-  fee: number | string;
-  status: "OPEN" | "CLOSED" | "CANCELLED" | string;
-
-  createdAt: string; // or Date
-
-  hostId: string;
-  host: IHost;
-
-  participants: IParticipant[];
-  reviews: IReview[];
-  payments: IPayment[];
+  location: string;
+  hobbies: string[];
+  interests: string[];
 }
 
+export interface Review {
+  id?: string;
+  rating?: number;
+  comment?: string;
+}
 
+export interface Payment {
+  status: "PENDING" | "PAID" | "FAILED";
+  amount: number;
+}
