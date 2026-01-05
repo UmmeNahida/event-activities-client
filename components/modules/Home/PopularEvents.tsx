3782a8de3c794c19c8a5a6f54bdd4be7ae88ea9c
Nahida Akther <ummenahida20@gmail.com>
2026-01-06 02:08:52 +0600

    refactor: simplify code stracture

--- components/modules/Home/PopularEvents.tsx
+++ components/modules/Home/PopularEvents.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { sampleEvents } from "./Banner"
 
 
@@ -14,7 +15,7 @@ const PopularEvents = () => {
             <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
               {sampleEvents.slice(0,2).map((e) => (
                 <article key={e.id} className="flex gap-4 items-center bg-white rounded-xl p-4 shadow-sm">
-                  <img src={e.img} alt={e.title} className="w-28 h-20 object-cover rounded-md" />
+                  <Image width={"300"}  height={"300"} src={e.img} alt={e.title} className="w-28 h-20 object-cover rounded-md" />
                   <div>
                     <h3 className="font-semibold">{e.title}</h3>
                     <div className="text-sm text-gray-500">{e.date} • {e.location}</div>
